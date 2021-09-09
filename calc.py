b7c013a0f6d6e2d60cec7ba9ea9d1e492d6404e0
Rbar70 <89280035+Rbar70@users.noreply.github.com>
2021-09-08 20:13:10 -0600

    Hope this works

---
+++ calc.py
@@ -0,0 +1,21 @@
+# what the VS Teminal window showed----
+#Windows PowerShell
+#Copyright (C) Microsoft Corporation. All rights reserved.
+
+#Try the new cross-platform PowerShell https://aka.ms/pscore6
+
+#PS S:\BMI6017\Week3\exerPythCreateBasic> & C:/Users/Robert/AppData/Local/Programs/Python/Python39/python.exe s:/BMI6017/Week3/exerPythCreateBasic/hello.py
+#Traceback (most recent call last):
+#  File "s:\BMI6017\Week3\exerPythCreateBasic\hello.py", line 1, in <module>
+#Warning:
+#not been activated.  Libraries may fail to load.  To activate this environment
+#please see https://conda.io/activation
+
+#Type "help", "copyright", "credits" or "license" for more information.
+#>>> first_number = int(input('Type the first number: ')) ;\
+#... second_number = int(input('Type the second number: ')) ;\
+#... print("The sum is: ", first_number + second_number)
+#Type the first number: 2
+#Type the second number: 3
+#The sum is:  5
+#>>>
---
+++ hello.py
@@ -0,0 +1,38 @@
+#Microsoft Windows [Version 10.0.19043.1165]
+#(c) Microsoft Corporation. All rights reserved.
+
+#S:\BMI6017\Week3\exerPythCreateBasic>Python
+#Python 3.8.8 (default, Apr 13 2021, 15:08:03) [MSC v.1916 64 bit (AMD64)] :: Anaconda, Inc. on win32
+
+#Warning:
+#This Python interpreter is in a conda environment, but the environment has
+#not been activated.  Libraries may fail to load.  To activate this environment
+#please see https://conda.io/activation
+
+#Type "help", "copyright", "credits" or "license" for more information.
+#>>> x = 1         # assign variable x the value 1
+#>>> y = x + 5     # assign variable y the value of x plus 5
+#>>> z = y         # assign variable z the value of y
+#>>> x = 1       # integer
+#>>> x = 1.0     # decimal (floating point)
+#>>> x = 1
+#>>> print(type(x)) # outputs: <class 'int'>
+#<class 'int'>
+#>>> x = 1.0
+#>>> print(type(x)) # outputs: <class 'float'>
+#<class 'float'>
+#>>> x = True
+#>>> print(type(x)) # outputs: <class 'bool'>
+#<class 'bool'>
+#>>> x = 'This is a string'
+#>>> print(x) # outputs: This is a string
+#This is a string
+#>>> print(type(x)) # outputs: <class 'str'>
+#<class 'str'>
+#>>> y = "This is also a string"
+#>>> x = 'Hello' + ' ' + 'World!'
+#>>> print(x) # outputs: Hello World!
+#Hello World!
+#>>> print('Hello World!')
+#Hello World!
+#>>>
